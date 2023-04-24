0b0cc0c2aaf65870f771c91c1e3d12b289f0b13b
Melvin <melvinnnamani@gmail.com>
2023-04-24 22:22:15 +0100

    added hostname

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 import cheerio from "cheerio";
 import cors from "cors";
+import URL from "url";
 
 const app = express();
 
@@ -13,13 +14,14 @@ app.get("/metadata", async (req, res) => {
   try {
     const response = await fetch(url);
     const html = await response.text();
+    const lpUrl = URL.parse(response.url);
     const $ = cheerio.load(html);
     
     const title = $('head title').text().trim();
     const description = $('head meta[name="description"]').attr('content');
     const image = $('head meta[property="og:image"]').attr('content');
 
-    res.json({ title, description, image, url });
+    res.json({ title, description, image, url, hostname: lpUrl.hostname, siteName: url});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch metadata." });

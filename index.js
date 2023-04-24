376447dcb2de67b83c690750a70d3f0a86ed7e56
Melvin <melvinnnamani@gmail.com>
2023-04-24 21:43:53 +0100

    set up link preview url

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+
+const app = express();
+
+app.get("/metadata", async (req, res) => {
+  const { url } = req.query;
+
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    
+    const title = $('head title').text().trim();
+    const description = $('head meta[name="description"]').attr('content');
+    const image = $('head meta[property="og:image"]').attr('content');
+
+    res.json({ title, description, image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch metadata." });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
